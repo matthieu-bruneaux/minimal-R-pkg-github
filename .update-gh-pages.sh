80c4b3f5b59dc958c2eb46b62df6ac808c47fd89
Matthieu Bruneaux <matthieu.bruneaux@gmail.com>
2020-03-25 15:53:22 +0200

    Try to update gh-pages using artifacts

--- .update-gh-pages.sh
+++ .update-gh-pages.sh
@@ -1,32 +1,12 @@
-# https://www.innoq.com/en/blog/github-actions-automation/
-
-# This script assumes that "make pkgdown" has been run before.
+# Modified from https://www.innoq.com/en/blog/github-actions-automation/
 
 set -eu
 
 repo_uri="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-remote_name="origin"
-main_branch="master"
-target_branch="gh-pages"
-
-cd "$GITHUB_WORKSPACE"
 
 git config user.name "$GITHUB_ACTOR"
 git config user.email "${GITHUB_ACTOR}@bots.github.com"
 
-git checkout "$main_branch"
-git branch -D "$target_branch" && git branch "$target_branch"
-git checkout "$target_branch"
-
-# Remove all non-docs files
-rm -fr man/ R/ tests/ vignettes/
-rm -f DESCRIPTION Makefile NAMESPACE _pkgdown.yml README.md
-# Move docs to the root folder
-ls
-tree
-mv docs/* .
-rm -fr docs/
-# Prepare the commit
 git add --force .
 
 git commit -m "Updated GitHub pages"
@@ -35,5 +15,5 @@ if [ $? -ne 0 ]; then
     exit 0
 fi
 
-git remote set-url "$remote_name" "$repo_uri"
-git push --force "$remote_name" "$target_branch"
+git remote set-url origin "$repo_uri"
+git push --force origin gh-pages
